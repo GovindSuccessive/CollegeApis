cb5bf2a809cdf9e6e3fb37dd0f8e58abe7bfc97a
GovindSuccessive <govind.sharma@successive.tech>
2023-12-18 14:20:54 +0530

    First Commit

---
+++ FirstWebAPiProject/Data/StudentStore.cs
@@ -0,0 +1,13 @@
+﻿using FirstWebAPiProject.Model.Dto;
+
+namespace FirstWebAPiProject.Data
+{
+    public class StudentStore
+    {
+        public static List<StudentDto> studentList = new List<StudentDto>()
+        {
+            new StudentDto() { Id=1,Name="Govind",PhoneNo="9832343293" },
+            new StudentDto() { Id=2,Name="Rishab",PhoneNo="8822940284"}
+        };
+    }
+}
---
+++ FirstWebAPiProject/Model/Dto/StudentDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebAPiProject.Model.Dto
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string PhoneNo { get; set; } = null!;
+    }
+}
